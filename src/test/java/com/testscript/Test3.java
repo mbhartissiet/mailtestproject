1ecf80cabbcecb25103c2ed78c9af5058f9f0eff
rjmahesh10 <maheshssietece@gmail.com>
2019-06-24 16:37:22 +0530

    MyFirstCommit

--- src/test/java/com/testscript/Test3.java
+++ src/test/java/com/testscript/Test3.java
@@ -8,5 +8,13 @@ public class Test3 {
 	public void testHe(){
 		System.out.println("I m test3 class");
 	}
+	@Test
+	public void testH(){
+		System.out.println("I m test4 class");
+	}
+	@Test
+	public void testk(){
+		System.out.println("I m test7 class");
+	}
 
 }
