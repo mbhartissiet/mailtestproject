a7d10b77afd900641cade9514b540910f25ef373
rjmahesh10 <maheshssietece@gmail.com>
2019-06-24 16:41:16 +0530

    NewFilecommit

---
+++ src/test/java/com/testscript/Test4.java
@@ -0,0 +1,15 @@
+package com.testscript;
+
+import org.testng.annotations.Test;
+
+public class Test4 {
+	
+	@Test
+	public void testH(){
+		System.out.println("I m test4 class");
+	}
+	public void testk(){
+		System.out.println("I m test5 class");
+	}
+
+}
